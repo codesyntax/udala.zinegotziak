3197c0eb7736e90cc04f23bf9e9bde55b45bb9ce
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-09-11 15:51:29 +0200

    feat: make content-type fields indexable

--- src/udala/zinegotziak/content/councillor.py
+++ src/udala/zinegotziak/content/councillor.py
@@ -18,6 +18,7 @@ from zope import schema
 from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
+from plone.app.dexterity import textindexer
 
 
 class ISocialLinkRowSchema(Interface):
@@ -96,12 +97,14 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("other_positions")
     other_positions = RichText(
         title=_("Other positions"),
         description=_("Write here any other position that this councillor holds"),
         required=False,
     )
 
+    textindexer.searchable("cv_text")
     cv_text = RichText(
         title=_("CV"),
         description=_(
@@ -110,12 +113,14 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("cv_file")
     cv_file = NamedBlobFile(
         title=_("CV file"),
         description=_("Upload the file with the CV of this councillor."),
         required=False,
     )
 
+    textindexer.searchable("earnings_text")
     earnings_text = RichText(
         title=_("Earnings"),
         description=_(
@@ -124,12 +129,14 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("earnings_file")
     earnings_file = NamedBlobFile(
         title=_("Earnings file"),
         description=_("Upload the file with the earning details of this councillor."),
         required=False,
     )
 
+    textindexer.searchable("assets_declaration_text")
     assets_declaration_text = RichText(
         title=_("Assets declaration"),
         description=_(
@@ -139,6 +146,7 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("assets_declaration_file")
     assets_declaration_file = NamedBlobFile(
         title=_("Assets declaration file"),
         description=_(
@@ -147,6 +155,7 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("activities_declaration_text")
     activities_declaration_text = RichText(
         title=_("Activities declaration"),
         description=_(
@@ -156,6 +165,7 @@ class ICouncillor(model.Schema):
         required=False,
     )
 
+    textindexer.searchable("activities_declaration_file")
     activities_declaration_file = NamedBlobFile(
         title=_("Activities declaration file"),
         description=_(
